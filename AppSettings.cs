59e2e180a468fceba26842a4d8146bfb2f6a0471
AlbertoGruning <alberto.gruning@ndvsistemas.com>
2021-09-23 20:49:50 -0300

    computer vision

--- AppSettings.cs
+++ AppSettings.cs
@@ -5,5 +5,12 @@
         //public static string ApiKey = "5449e07d53bc4f2e85a585d4bf72c8c2";
 
         public static string ApiKeySearch = "0b0d36e8da3a440db1fdd65c04f6dac7";
+        // cv  6af1c2ecbaae455694fed243cd6e1385
+
+        //cv teacher 2a68dc731e24451c9d1e1130d3db6536
+
+        public static string ApiKeyCV = "6af1c2ecbaae455694fed243cd6e1385";
+
+        public static string EndpointCV = "https://c-vision-2021.cognitiveservices.azure.com/";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.Bing.VisualSearch;
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using VisualSearchEjemplo;
 
-FileStream stream = new FileStream("../../../car.jpg", FileMode.Open);
+FileStream stream = new FileStream("../../../imagen.jpg", FileMode.Open);
 
-var cliente = new VisualSearchClient(
-    new ApiKeyServiceClientCredentials(AppSettings.ApiKeySearch)
+//var cliente = new VisualSearchClient(
+//    new Microsoft.Bing.VisualSearch.ApiKeyServiceClientCredentials(AppSettings.ApiKeySearch)
+//    );
+//Console.WriteLine("inicia llamada al visual search");
+//var resultados = await cliente.Images.VisualSearchMethodAsync(image: stream);
+//Console.WriteLine("llamada terminada Visual Seach");
+//if (resultados.Tags.Count > 0) {
+//    foreach (var tag in resultados.Tags) {
+//        Console.WriteLine(tag.Actions[0].DisplayName);
+//    }
+//}
+
+Console.WriteLine("inicia llamada al Computer ");
+var clienteCV = new ComputerVisionClient(
+    new Microsoft.Azure.CognitiveServices.Vision.ComputerVision.ApiKeyServiceClientCredentials(AppSettings.ApiKeyCV)
     );
-Console.WriteLine("inicia llamada");
-var resultados = await cliente.Images.VisualSearchMethodAsync(image: stream);
-Console.WriteLine("llamada terminada");
-if (resultados.Tags.Count > 0) {
-    foreach (var tag in resultados.Tags) {
-        Console.WriteLine(tag.Actions[0].DisplayName);
+clienteCV.Endpoint = AppSettings.EndpointCV;
+
+var resultadosCV = await clienteCV.DescribeImageInStreamAsync(stream);
+
+if (resultadosCV.Captions.Count > 0) {
+    foreach (var cap in resultadosCV.Captions) {
+        Console.WriteLine(cap.Text);
     }
 }
+
+Console.WriteLine("fin llamada al Computer ");
