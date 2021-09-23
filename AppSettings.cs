fe0f88afd103dca881b6d7741697bd9d8ad00963
AlbertoGruning <alberto.gruning@ndvsistemas.com>
2021-09-23 20:15:54 -0300

    Add project files.

---
+++ AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace VisualSearchEjemplo
+{
+    public static class AppSettings
+    {
+        //public static string ApiKey = "5449e07d53bc4f2e85a585d4bf72c8c2";
+
+        public static string ApiKeySearch = "0b0d36e8da3a440db1fdd65c04f6dac7";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Bing.VisualSearch;
+using VisualSearchEjemplo;
+
+FileStream stream = new FileStream("../../../car.jpg", FileMode.Open);
+
+var cliente = new VisualSearchClient(
+    new ApiKeyServiceClientCredentials(AppSettings.ApiKeySearch)
+    );
+Console.WriteLine("inicia llamada");
+var resultados = await cliente.Images.VisualSearchMethodAsync(image: stream);
+Console.WriteLine("llamada terminada");
+if (resultados.Tags.Count > 0) {
+    foreach (var tag in resultados.Tags) {
+        Console.WriteLine(tag.Actions[0].DisplayName);
+    }
+}
